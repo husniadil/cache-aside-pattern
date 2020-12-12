7d953a148989479e16c223b12489edf8b9944d7f
Husni Adil Makmur <husni.adil@gmail.com>
2020-12-12 13:38:51 +0700

    Cache stampede simulation

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 
 	"github.com/husniadil/cache-aside-pattern/repository"
@@ -15,29 +16,21 @@ func main() {
 
 	id := "2c1b7cd2-0420-4b73-a3f9-734504842fb9"
 
+	var wg sync.WaitGroup
+	wg.Add(5)
 	var names []string
 	for i := 0; i < 5; i++ {
-		name, err := repo.DoAnExpensiveQuery(id)
-		if err != nil {
-			fmt.Printf("Error loading [%s]: %s", id, err.Error())
-			continue
-		}
-		names = append(names, *name)
+		go func(id string) {
+			defer wg.Done()
+			name, err := repo.DoAnExpensiveQuery(id)
+			if err != nil {
+				fmt.Printf("Error loading [%s]: %s", id, err.Error())
+				return
+			}
+			names = append(names, *name)
+		}(id)
 	}
-
-	// wait for cache expiration and we'll see a cache miss
-	fmt.Printf("-------------- waiting cache expiration --------------\n\n")
-
-	time.Sleep(ttl)
-
-	for i := 0; i < 5; i++ {
-		name, err := repo.DoAnExpensiveQuery(id)
-		if err != nil {
-			fmt.Printf("Error loading [%s]: %s", id, err.Error())
-			continue
-		}
-		names = append(names, *name)
-	}
+	wg.Wait()
 
 	fmt.Println("Result:", names)
 }
